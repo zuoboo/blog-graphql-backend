c7129c6b983f97ef5ce80ed42cee1325e159c592
zuoboo <naoya.saiki@rightcode.co.jp>
2023-07-02 22:32:35 +0900

    context

--- src/index.js
+++ src/index.js
@@ -2,37 +2,31 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { readFileSync } from "fs";
+import { PrismaClient } from "@prisma/client";
 
-// Hackernewsの投稿
-let links = [
-  {
-    id: "link-0",
-    description: "Fullstack tutorial for GraphQL",
-    url: "www.howtographql.com",
-  },
-];
+const prisma = new PrismaClient();
 
 // schema.graphqlファイルを読み込む
-const typeDefs = readFileSync("./src/schema.graphql", { encoding: 'utf-8' });
+const typeDefs = readFileSync("./src/schema.graphql", { encoding: "utf-8" });
 
 // リゾルバを定義する
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
 
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-
-      links.push(link);
-      return link;
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          description: args.description,
+          url: args.url,
+        },
+      });
+      return newLink;
     },
   },
 };
@@ -45,6 +39,10 @@ const server = new ApolloServer({
 
 // startStartaloneServer関数にApolloServerインスタンスを渡す
 const { url } = await startStandaloneServer(server, {
+  // contextを追加し、リゾルバに渡す
+  context: async ({ req, res }) => ({
+    prisma,
+  }),
   listen: { port: 4000 },
 });
 
